d3f4bdfaec264655fd16e89da6c1d83cb9a82ab8
daileytj <daileytj89@gmail.com>
2021-01-28 13:17:24 -0500

    Remove custom menu example

--- components/user-menu-example.tsx
+++ components/user-menu-example.tsx
@@ -1,52 +1,15 @@
-import { InfoListItem, InfoListItemProps, UserMenu, wrapIcon } from '@pxblue/react-native-components';
-import React, { useState } from 'react';
+import { InfoListItemProps, UserMenu, wrapIcon } from '@pxblue/react-native-components';
+import React from 'react';
 import { Image, View } from 'react-native';
-import { Avatar, Card, Divider } from 'react-native-paper';
+import { Avatar, Card } from 'react-native-paper';
 import * as Colors from '@pxblue/colors';
 import MatIcon from 'react-native-vector-icons/MaterialIcons';
 const VpnKeyIcon = wrapIcon({ IconClass: MatIcon, name: 'vpn-key', flip: false });
 const SettingsIcon = wrapIcon({ IconClass: MatIcon, name: 'settings', flip: false });
 const ExitToAppIcon = wrapIcon({ IconClass: MatIcon, name: 'exit-to-app', flip: false });
-const NumericOneBoxIcon = wrapIcon({ IconClass: MatIcon, name: 'looks-one', flip: false });
-const NumericTwoBoxIcon = wrapIcon({ IconClass: MatIcon, name: 'looks-two', flip: false });
 const avatarTestImage = require('../assets/images/test-avatar.png');
 
 export const UserMenuExample: React.FC = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const closeMenu = (): void => {
-        setMenuOpen(false);
-    };
-
-    const customMenu = (): JSX.Element => (
-        <View>
-            <InfoListItem
-                hidePadding
-                leftComponent={
-                    <Avatar.Icon
-                        icon="account-circle"
-                        size={40}
-                        color={Colors.white[50]}
-                        style={{ backgroundColor: Colors.red[500], marginLeft: 16 }}
-                    />
-                }
-                title={'Custom Menu Title'}
-                subtitle={'Custom Menu Subtitle'}
-            />
-            <Divider />
-            <InfoListItem
-                title={'Custom Menu Info List Item 1'}
-                IconClass={NumericOneBoxIcon}
-                onPress={(): void => closeMenu()}
-            />
-            <InfoListItem
-                title={'Custom Menu Info List Item 2'}
-                IconClass={NumericTwoBoxIcon}
-                onPress={(): void => closeMenu()}
-            />
-        </View>
-    );
-
     const menuItems: InfoListItemProps[] = [
         { title: 'Change Password', IconClass: VpnKeyIcon },
         { title: 'Preferences', IconClass: SettingsIcon },
@@ -64,11 +27,9 @@ export const UserMenuExample: React.FC = () => {
                 }}
             >
                 <UserMenu
-                    menu={customMenu()}
-                    menuOpen={menuOpen}
-                    toggleMenu={(): void => {
-                        setMenuOpen(!menuOpen);
-                    }}
+                    menuTitle={'John Smith'}
+                    menuSubtitle={'j.smith@example.com'}
+                    menuItems={menuItems}
                     avatar={
                         <Avatar.Icon
                             icon="account-circle"
