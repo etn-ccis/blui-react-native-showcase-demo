67840eda82b414c1fae546c245062505001e0e36
jeffvg <jeffvgreiner@eaton.com>
2021-08-25 14:14:03 -0400

    update mobile stepper test

--- __tests__/mobile-stepper-test.tsx
+++ __tests__/mobile-stepper-test.tsx
@@ -1,18 +1,28 @@
 import 'react-native';
 import React from 'react';
 import { MobileStepperExample } from '../components/mobile-stepper-example';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { blue  } from '@pxblue/react-native-themes';
 import renderer from 'react-test-renderer';
 jest.useFakeTimers();
+jest.mock('react-native-safe-area-context', () => ({
+    useSafeAreaInsets: (): { top: number; left: number; right: number; bottom: number } => ({
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    }),
+}));
 
 describe('Kitchen sink snapshot', () => {
     test('Renders correctly and snapShot matches', ()=>{
     const snap = renderer.create(
-        <SafeAreaProvider>
-            <SafeAreaView>
+        <ThemeContext.Provider value={{ theme: 'light', setTheme: () => {} }}>
+            <PaperProvider theme={blue}>
                 <MobileStepperExample />
-            </SafeAreaView>
-        </SafeAreaProvider>
+            </PaperProvider>
+        </ThemeContext.Provider>
     ).toJSON();
     expect(snap).toMatchSnapshot();
 })
