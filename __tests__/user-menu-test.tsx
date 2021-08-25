e70c08f5d49cb3cdc987de07e97ba2fc6189cf66
jeffvg <jeffvgreiner@eaton.com>
2021-08-25 14:17:00 -0400

    update user menu test

--- __tests__/user-menu-test.tsx
+++ __tests__/user-menu-test.tsx
@@ -1,13 +1,28 @@
 import 'react-native';
 import React from 'react';
 import { UserMenuExample } from '../components/user-menu-example';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { blue  } from '@pxblue/react-native-themes';
 import renderer from 'react-test-renderer';
 jest.useFakeTimers();
+jest.mock('react-native-safe-area-context', () => ({
+    useSafeAreaInsets: (): { top: number; left: number; right: number; bottom: number } => ({
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    }),
+}));
 
 describe('Kitchen sink snapshot', () => {
     test('Renders correctly and snapShot matches', ()=>{
     const snap = renderer.create(
-            <UserMenuExample onToggleRTL={() => {}} onToggleTheme={() => {}} />
+        <ThemeContext.Provider value={{ theme: 'light', setTheme: () => {} }}>
+            <PaperProvider theme={blue}>
+                <UserMenuExample onToggleRTL={() => {}} onToggleTheme={() => {}} />
+            </PaperProvider>
+        </ThemeContext.Provider>
     ).toJSON();
     expect(snap).toMatchSnapshot();
 })
