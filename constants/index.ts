7b47c0b3307ac539b5841f4fc70dea4aca11aa34
Suraj Karambe <bkarambe@gmail.com>
2022-12-09 18:20:10 +0530

    Updated min font scale value

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 export const MAX_FONT_SCALE = 1.3;
-export const MIN_FONT_SCALE = 1;
-export const DISABLE_FONT_SCALE = true;
+export const MIN_FONT_SCALE = 0.5;
+export const DISABLE_FONT_SCALE = false;
