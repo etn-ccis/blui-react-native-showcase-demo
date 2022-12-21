ac884ad8a36e4f2331533b11ad61d7015adc1340
Suraj Karambe <bkarambe@gmail.com>
2022-12-21 12:17:27 +0530

    Passed values to font scale provider

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
-export const MAX_FONT_SCALE = 1.3;
-export const MIN_FONT_SCALE = 0.5;
+export const MAX_FONT_SCALE = 100;
+export const MIN_FONT_SCALE = 0.01;
 export const DISABLE_FONT_SCALE = false;
--- index.tsx
+++ index.tsx
@@ -9,7 +9,7 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import React, { useState } from 'react';
 import { ThemeContext, ThemeType } from './contexts/ThemeContext';
 import { blue, blueDark } from '@brightlayer-ui/react-native-themes';
-import { FontScaleContext } from '@brightlayer-ui/react-native-components';
+import { FontScaleProvider } from '@brightlayer-ui/react-native-components';
 import { DISABLE_FONT_SCALE, MAX_FONT_SCALE, MIN_FONT_SCALE } from './constants';
 
 const wrapper = (): JSX.Element => {
@@ -18,11 +18,13 @@ const wrapper = (): JSX.Element => {
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
             <PaperProvider theme={theme === 'light' ? blue : blueDark}>
-                <FontScaleContext.Provider
-                    value={{ maxScale: MAX_FONT_SCALE, minScale: MIN_FONT_SCALE, disableScaling: DISABLE_FONT_SCALE }}
+                <FontScaleProvider
+                    disableScaling={DISABLE_FONT_SCALE}
+                    maxScale={MAX_FONT_SCALE}
+                    minScale={MIN_FONT_SCALE}
                 >
                     <MainRouter />
-                </FontScaleContext.Provider>
+                </FontScaleProvider>
             </PaperProvider>
         </ThemeContext.Provider>
     );
