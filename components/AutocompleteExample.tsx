785ec8e6b35bdab2bcf7da0383befd64dc34c1de
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2024-06-24 17:41:17 +0530

    bug fix

--- components/AutocompleteExample.tsx
+++ components/AutocompleteExample.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AutoComplete } from '@brightlayer-ui/react-native-components';
-import { Card } from 'react-native-paper';
+import { Card, TextInput } from 'react-native-paper';
 import { useExtendedTheme } from '@brightlayer-ui/react-native-themes';
 
 export const AutoCompleteExample: React.FC = () => {
     const theme = useExtendedTheme();
+    const [chipValue, setChipValue] = useState(['Tag1']);
     return (
         <Card style={{ marginTop: 20 }}>
             <Card.Title title="AutoComplete" />
             <Card.Content>
+                <TextInput label={'label'} />
                 <AutoComplete
                     helperText="Helper text"
-                    value={['Tag1']}
-                    allowCustomtag={true}
+                    value={chipValue}
+                    // allowCustomtag={true}
+                    onChange={(arr) => {
+                        setChipValue(arr);
+                    }}
+                    onDelete={(item) => {
+                        let arr = chipValue.filter((str) => str !== item);
+                        setChipValue(arr);
+                    }}
                     label="Label"
                     options={['Tag1', 'Tag2', 'Tag3', 'Tag4']}
                 />
